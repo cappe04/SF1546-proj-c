720d69b83e2a6da91f632c5753b16e56d3352620
Ambjorn-Hogmark <142985496+Ambjorn-Hogmark@users.noreply.github.com>
2025-04-20 18:17:13 +0200

    Inskjutning på slutet löser "hoppande" fel
    Lade till inskjutning på slutet så att sista x värdet = bordskanten. Behövde lägga in en tolerans i secant annars får man NaN

--- A.m
+++ A.m
@@ -2,13 +2,20 @@ clear; format long;
 
 vars = get_vars(ones(1, 8));
 
-h = 0.001;
+h = 0.0001;
 
 [u1, p_crit, net_dist, e1, ~, ~] = step_solve(vars, [0, 4, vars.y_start, 0], h);
 
 % FELSKATTNING
-[u2, ~, ~, e2, ~, ~] = step_solve(vars, [0, 4, vars.y_start, 0], 2*h);
-e_tot = abs(u1(end, :) - u2(end, :)) + e1 + e2;
+% [u2, ~, ~, e2, ~, t2] = step_solve(vars, [0, 4, vars.y_start, 0], 2*h);
+% e_tot = abs(u1(end, :) - u2(end, :)) + e1 + e2;
+
+[u2, ~, ~, e2, ~, t2] = step_solve(vars, [0, 4, vars.y_start, 0], 2*h);
+[u3, ~, ~, e3, ~, t3] = step_solve(vars, [0, 4, vars.y_start, 0], 4*h);
+
+E1 = abs(u1(end,:)-u2(end,:))
+E2 = abs(u2(end,:)-u3(end,:))
+E_ratio = E2/E1
 
 % VALIDERING AV SERV
 valid_serve = validate_serve(vars, p_crit, net_dist);
@@ -16,6 +23,7 @@ valid_serve = validate_serve(vars, p_crit, net_dist);
 % PLOT
 plot_solution(u1, p_crit, net_dist, valid_serve);
 
+
 % VÄRDEN
-PUNKTER = p_crit(1:2, 1)
-ERROR = p_crit(1:2, 2) + e_tot(1)
+% PUNKTER = p_crit(1:2, 1);
+% ERROR = p_crit(1:2, 2) + e_tot(1);
--- adaptive_step.m
+++ adaptive_step.m
@@ -7,7 +7,7 @@ function [t_new, u_new, e_u] = adaptive_step(f, condition, u1, u_prev, t1, h)
             y = condition(u);
         end
 
-        dt = secant(@minimize, 0.2*H, 0.8*H, 0, 4); % Billig att göra, med iter=4 blir felet 0
+        dt = secant(@minimize, 0.2*H, 0.8*H, 1*0.5e-15, 4); % Billig att göra, med iter=4 blir felet 0
         u = rk4_step(f, t, u1, dt);
         t_new = t + dt;
     end
